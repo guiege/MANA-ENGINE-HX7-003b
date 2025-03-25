58708505d4f826aad18309fa219727b6a711d440
guiege <schmitzc321@gmail.com>
2025-03-25 16:24:22 -0400

    new

---
+++ include/headers/Spritefont.h
@@ -0,0 +1,41 @@
+#ifndef SPRITEFONT_H
+#define SPRITEFONT_H
+
+#include "Spritesheet.h" 
+
+class Spritefont : public Spritesheet {
+public:
+	std::vector<int> getAlphabetIndices(const std::string& str) {
+	    std::vector<int> indices;
+
+	    for (char c : str) {
+	    	if (isspace(c)) {
+	    		indices.push_back(-1);
+	    	}
+	        if (std::isalpha(c)) {
+	            indices.push_back(std::tolower(c) - 'a');
+	        }
+	    }
+
+	    return indices;
+	}
+
+	std::vector<int> codes;
+	std::vector<int> currentCodes;
+	unsigned int spacing = 5;
+	unsigned int spaceWidth = 7;
+	unsigned int scale = 10;
+
+	unsigned int startTick = 20;
+	unsigned int index = 0;
+
+	bool animated = true;
+
+	Spritefont(const std::string& text, Texture& texture, const char* file, const float xpos, const float ypos, const float width, const float height, const float rot, glm::vec4 color = glm::vec4(1.0f));
+
+	void draw(Renderer* renderer);
+	void update(int tick);
+	void playAnim(int sTick);
+};
+
+#endif
---
+++ src/spritefont.cpp
@@ -0,0 +1,53 @@
+#include "Spritefont.h"
+
+Spritefont::Spritefont(const std::string& text, Texture& texture, const char* file, const float xpos, const float ypos, const float width, const float height, const float rot, glm::vec4 color)
+: Spritesheet(texture, file, xpos, ypos, width, height, rot, color)
+{
+	codes = getAlphabetIndices(text);
+	if(!animated){
+		currentCodes = codes;
+	}
+}
+
+void Spritefont::draw(Renderer* renderer) 
+{
+	if(renderer->isBatch){
+		int oldx = pos.x;
+		for(int i = 0; i < currentCodes.size(); i++){
+			if(currentCodes[i] >= 0){
+				SetFrame(currentCodes[i]);
+				renderer->DrawQuadAtlas(texture, glm::vec2(curClip.x, curClip.y), glm::vec2(curClip.w, curClip.h), curClip.rotated, pos + glm::vec2(anchorPosition - curClip.sourceX - curClip.w , curClip.sourceY), color, glm::vec2(scale));
+				pos.x += spacing*scale;
+			}else{
+				if(currentCodes[i] == -1)
+					pos.x += spaceWidth*scale;
+			}
+		}
+		pos.x = oldx;
+	}
+	else{
+		// LMAO renderer->DrawTextureAtlas(texture, glm::vec2(curClip.x, curClip.y), glm::vec2(curClip.w, curClip.h), curClip.rotated, (pos + glm::vec2(curClip.sourceX, curClip.sourceY)), color);
+	}
+}
+
+void Spritefont::playAnim(int sTick)
+{
+	index = 0;
+	currentCodes.clear();
+	startTick = sTick;
+}
+
+void Spritefont::update(int tick)
+{
+	int diff = tick-startTick;
+	if(!animated || diff < 0){
+		return;
+	}
+	int proc = (diff)%5;
+	if(proc == 0){
+		if(index < codes.size()){
+			currentCodes.push_back(codes[index]);
+			index++;
+		}
+	}
+}
